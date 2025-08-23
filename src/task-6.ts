aed3a51d6912602b0c6b0b1a564dca7ac0a7d9a3
VitaKoval27 <talisman1980@gmail.com>
2025-08-23 18:13:16 +0100

    homeWork1

---
+++ src/task-6.ts
@@ -0,0 +1,13 @@
+// Зроби функцію узагальненою, використовуючи тип T, щоб вона працювала з масивами будь-якого типу.
+// Додай явну типізацію дженериків у виклики функцій.
+// Переконайся, що тип елемента, який повертається, точно відповідає типу елементів у масиві.
+// Перевір, що TypeScript не дозволяє передати масив змішаних типів без відповідного типу.
+
+
+function getFirstElement<T>(arr:T[]):T {
+  return arr[0];
+}
+
+console.log(getFirstElement<number>([1, 2, 3]));           // 1
+console.log(getFirstElement<string>(["a", "b", "c"]));     // "a"
+console.log(getFirstElement<boolean>([true, false, true])); // true
